f54a977a369163b27cdd2180d1db89063872d29e
selenasunny <alena_po112@tut.by>
2017-03-17 13:01:37 +0300

    implementation

---
+++ index.php
@@ -0,0 +1,407 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE9" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="index, follow" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/custom.css" />
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link href='https://fonts.googleapis.com/css?family=Cuprum:400,400italic,700,700italic&subset=latin,latin-ext,cyrillic' rel='stylesheet' type='text/css'>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body data-spy="scroll" data-target="#navbar-scroll">
+    <!--ВЕРХНИЙ САЙЛБАР-->
+    <div class="navbar-wrapper">
+        <div class="container-fluid">
+            <nav class="navbar navbar-fixed-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">Меню <span class="caret"></span>
+                            <span class="sr-only">Развернуть</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Logo</a>
+
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Для Учеников <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Почему это работает?</a></li>
+                                    <li><a href="#">Акции</a></li>
+                                </ul>
+                            </li>
+
+                            <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Для преподавателей <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Стать преподавателем</a></li>
+                                    <li><a href="#">Условия сотрудничества</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Тарифы</a>
+                            </li>
+                            <li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">FAQ <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Инструкция по работе с сервисом</a></li>
+                                    <li><a href="#">Частые вопросы</a></li>
+                                    <li><a href="#">Задать вопрос</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">О сервисе </a>
+
+                            </li>
+
+                        </ul>
+                        <ul class="nav navbar-nav pull-right">
+                            <li class="phone"><img src="" alt="" class="phone-img"><a class="phone-backcall">+7 (812) 777-77-77</a><small class="pull-right phone-small">с 09:00 до 20:00</small></li>
+                            <li class=" dropdown">
+                                <a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="" class="auth-img">Вход <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Для учеников</a></li>
+                                    <li><a href="#">Для преподавателей</a></li>
+                                </ul>
+                            </li>
+                            <li class=" dropdown">
+                                <a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img src="" class="reg-img">Регистрация <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Для учеников</a></li>
+                                    <li><a href="#">Для преподавателей</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!--КОНЕЦ ВЕРХНИЙ САЙЛБАР-->
+    <!--СЛАЙДЕР-->
+    <div class="slider">
+        <div class="slide-list">
+            <div class="slide-wrap">
+                <div class="slide-item">
+                    <img src="img/slide.jpg" alt="" />
+                </div>
+                <div class="slide-item">
+                    <img src="img/slide2.jpg" alt="" />
+                </div>
+
+
+            </div>
+            <div class="clear"></div>
+        </div>
+        <div name="prev" class="navy prev-slide"></div>
+        <div name="next" class="navy next-slide"></div>
+
+    </div>
+    <div class="container-fluid top-social">
+        <div class="container">
+            <row>
+                <div class="col-xs-12 col-sm-12 col-md-7 text-right ab">
+                    <p>Сервис индивидуального обучения иностранным языкам</p>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-5 text-right">
+                    <p>Мы в соцсетях:
+                        <a href="/" class="vk"><img src="img/vk.png"></a>
+                        <a href="/" class="fb"><img src="img/fb.png"></a>
+                        <a href="/" class="yout"><img src="img/yout.png"></a>
+                        <a href="/" class="tw"></a><img src="img/tw.png"></p>
+                </div>
+            </row>
+        </div>
+    </div>
+
+    <!--ВЫБОР ЯЗЫКА ДЛЯ ИЗУЧЕНИЯ-->
+    <div class="container-fluid select-lang">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-md-5 slogan-p">
+
+                    <p>
+                        Один из лучших сервисов которые я видел=)
+                    </p>
+                    <small class="pull-right"><i>Александр Воронков &mdash; основатель Terra Development Group</i></small>
+
+                </div>
+                <div class="col-xs-12 col-md-7">
+                    <p class="vib">Выбор языка для изучения:</p>
+                    <p class="text-right">
+                        <a href="" class="lang-a eng">Английский</a>
+                        <a href="" class="lang-a ger">Немецкий</a>
+                        <a href="" class="lang-a france">Французский</a>
+                        <a href="" class="lang-a spain">Испанский</a>
+                        <a href="" class="lang-a italy">Итальянский</a>
+                        <a href="" class="lang-a all-language">Все языки</a>
+
+
+
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!--КОНЕЦ ВЫБОР ЯЗЫКА ДЛЯ ИЗУЧЕНИЯ-->
+    <!--КОНЕЦ СЛАЙДЕР-->
+
+    <!--ГЛАВНОЕ МЕНЮ--->
+    <div class="navbar-wrapper main-nav">
+        <div class="container-fluid">
+            <nav class="navbar navbar-fixed">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1" aria-expanded="false" aria-controls="navbar">Меню <span class="caret"></span>
+                            <span class="sr-only">Развернуть</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+
+
+                    </div>
+                    <div id="navbar1" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#" class="">Главная</a></li>
+                            <li class="active"><a href="#" class="">Как это работает?</a></li>
+                            <li class="active"><a href="#" class="">Преимущества</a></li>
+                            <li class="active"><a href="#" class="">Преподаватели</a></li>
+                            <li class="active"><a href="#" class="">Уроки</a></li>
+                            <li class="active"><a href="#" class="">Тарифы и цены</a></li>
+                            <li class="active"><a href="#" class="">Акции и скидки</a></li>
+                            <li class="active"><a href="#" class="">Обратная связь</a></li>
+
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <!--ПОЧЕМУ МЫ ЛУЧШИЕ-->
+    <div class="container-fluid section2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>почему наш сервис лучше?</h2>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-3" text-center>
+                    <div class="tiz"><img src="img/tiz1.png" alt="" class="tiz_img">
+                        <p>с нами легко, понятно
+                            <br> и удобно</p>
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-6 col-md-3" text-center>
+                    <div class="tiz"><img src="img/tiz2.png" alt="" class="tiz_img">
+                        <p>проверенные
+                            <br> преподаватели</p>
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-6 col-md-3" text-center>
+                    <div class="tiz"><img src="img/tiz3.png" alt="" class="tiz_img">
+                        <p>постоянное обновление
+                            <br> материала</p>
+                    </div>
+                </div>
+
+                <div class="col-md-3" text-center>
+                    <div class="tiz"><img src="img/tiz4.png" alt="" class="tiz_img">
+                        <p>низкие цены и
+                            <br> гибкая система скидок</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-4" text-right>
+                    <div class="tiz"><img src="img/tiz5.png" alt="" class="tiz_img">
+                        <p>с нами язык
+                            <br> учится быстрее</p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4" text-center>
+                    <div class="tiz"><img src="img/tiz6.png" alt="" class="tiz_img">
+                        <p>низкие цены и
+                            <br> гибкая система скидок</p>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-4 col-md-4" text-left>
+                    <div class="tiz"><img src="img/tiz7.png" alt="" class="tiz_img">
+                        <p>низкие цены и
+                            <br> гибкая система скидок</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!--НАШИ ТАРИФы-->
+    <div class="container-fluid section3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 ">
+                    <h2>наши тарифы</h2>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="block">
+                        <h4>Базовый</h4>
+                        <p>Доступ к материалам сайта</p>
+                        <p>10 минут общения с преподавателем</p>
+                        <div class="money">200 рублей</div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="block">
+                        <h4>10 минут</h4>
+                        <p>Доступ к материалам сайта</p>
+                        <p>10 минут в день для общения</p>
+                        <div class="money">2000 руб/мес</div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-12 col-sm-4">
+                    <div class="block">
+                        <h4>1 час</h4>
+                        <p>Доступ к материалам сайта</p>
+                        <p>1 час для общения</p>
+                        <div class="money">10000 руб/мес</div>
+                    </div>
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="read_more"><a href="#">подробнее</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--НОВОСТИ, ОТЗЫВЫ, СПЕЦПРЕДЛОЖЕНИЕ-->
+    <div class="container-fluid section4">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-4">
+                    <div class="block_section4">
+                    <img src="img/black_line.png" alt="" class="black_line" >
+                        <h3>новости</h3>
+                        <div class="block_section4_border"><img src="img/img_news.png" alt="" class="img_news">
+                            <p>Какой то текст новости размещается здесь и описывает очень интересную новость. В основном это делается для сео, потому как врятли кто-то будет читать эти новости. Но может быть кто-то и прочитает, почему нет, ведь это действительно интересно!</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="col-xs-12 col-sm-4 col-md-4">
+                        <div class="block_section4">
+                        <img src="img/black_line.png" alt="" class="black_line" >
+                            <h3>отзывы о сервисе</h3>
+                            <div class="block_section4_border">
+                                <p>Я и подумать не мог, что учить язык может быть так легко и удобно! Я просто сижу домаи общаюсь с носителем языка, невероятно=) Раньше я никак не мог начать учить французский язык, а теперь меня учит невероятная француженка)) спасибо команде проекта за качественно новый подход к изучению иностранных языков! <span>Олег, 23 года</span></p>
+                                <a href="#">
+                                    <p>Читать все отзывы</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-4 col-md-4">
+                        <div class="block_section4">
+                            <h3>Спецпредложение</h3>
+                            <div class="block_section4_sale">
+                                <h3>скидка 20% на первый час</h3>
+                            </div>
+                            <p>Сделайте репост этой записи и скидка ваша!</p>
+                            <img src="img/time.png" alt="">
+                            <p>Время акции органичено</p>
+                            <img src="img/icon_pack.png" alt="">
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--FOOTER-->
+
+    <footer>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2 col-xs-12 col-sm-1">
+                        <img src="img/logo_footer.png" alt="">
+                    </div>
+                    <div class="col-md-6 col-xs-12 col-sm-8">
+                        <img src="img/red_line.png" alt="" class="red_line" >
+                        <div class="footer_menu">
+                            <ul>
+                                <li>преподаватели</li>
+                                <li>уроки</li>
+                                <li>тарифы</li>
+                                <li>поддержка</li>
+                                <li>акции</li>
+                            </ul>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. AeneanLorem ipsum dolor sit amet, consectetur adipiscing elit. AeneanLorem ipsum dolor sit amet, consectetur adipiscing elit. AeneanLorem ipsum dolor sit amet, consectetur adipiscing elit. AeneanLorem ipsum dolor sit amet, consectetur adipiscing elit. AeneanLorem ipsum dolor sit amet, consectetur adipiscing elit. AeneanLorem ipsum dolor sit amet, consectetur adipiscing elit. AeneanLorem ipsum dolor sit amet, consectetur adipiscing elit. AeneanLorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean</p>
+                     
+                    </div>
+
+
+                    <div class="col-md-4 col-xs-12 col-sm-3">
+                        <div class="footer_right">
+                            <div class="phone_number">+7(812)777-77-77</div>
+                            <p>c 09:00 до 21:00</p></div>
+                            <div class="ring"><a href="#">Заказать звонок</a></div>
+                            <ul>
+                                <li>Правовая информация</li>
+                                <li>Карта сайта</li>
+                            </ul>
+                            <div class="copyright">
+                                <p>Название сайта 2015 (с)
+                                    <br>
+                                </p>
+                                Разработка сайта:<a href="#">TerraCorp</a>
+                                <div class="rights">Все права защищены</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </footer>
+
+
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+</body>
+
+</html>
